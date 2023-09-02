76feeffc204ed36683d3edeb338334a3282ba117
Oliver Benns <oliverbenns@gmail.com>
2023-09-02 22:42:51 +0100

    x

--- scraper/internal/service/service.go
+++ scraper/internal/service/service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/oliverbenns/uk-housing-developments/scraper/internal/scraper"
@@ -21,29 +23,50 @@ func (s *Service) Run() ([]byte, error) {
 	}
 
 	serviceResults := []Result{}
+	mu := sync.Mutex{}
+	var wg sync.WaitGroup
+	wg.Add(len(scrapers))
+	errs := []error{}
 
-	for _, scraper := range scrapers {
-		startTime := time.Now()
-		log.Printf("scraping %s", scraper.Name())
-		results, err := scraper.Scrape()
-		if err != nil {
-			return nil, fmt.Errorf("could not scrape %s: %w", scraper.Name(), err)
-		}
+	for _, sc := range scrapers {
+		go func(closureScraper scraper.Scraper) {
+			defer wg.Done()
 
-		for _, result := range results {
-			serviceResult := Result{
-				Builder:  scraper.Name(),
-				Name:     result.Name,
-				Url:      result.Url,
-				Location: result.Location,
+			startTime := time.Now()
+			log.Printf("scraping %s", closureScraper.Name())
+			results, err := closureScraper.Scrape()
+
+			mu.Lock()
+			defer mu.Unlock()
+
+			if err != nil {
+				wrappedErr := fmt.Errorf("could not scrape %s: %w", closureScraper.Name(), err)
+				errs = append(errs, wrappedErr)
+				return
 			}
 
-			serviceResults = append(serviceResults, serviceResult)
-		}
+			// Currently locked through this, could improve.
+			for _, result := range results {
+				serviceResult := Result{
+					Builder:  closureScraper.Name(),
+					Name:     result.Name,
+					Url:      result.Url,
+					Location: result.Location,
+				}
 
-		elapsed := time.Now().Sub(startTime)
-		log.Printf("finished scraping %s, took %s", scraper.Name(), elapsed.String())
+				serviceResults = append(serviceResults, serviceResult)
+			}
 
+			elapsed := time.Now().Sub(startTime)
+			log.Printf("finished scraping %s, took %s", closureScraper.Name(), elapsed.String())
+
+		}(sc)
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 {
+		return nil, errors.Join(errs...)
 	}
 
 	out := Out{
